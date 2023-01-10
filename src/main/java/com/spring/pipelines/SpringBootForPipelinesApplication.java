fb362cdeb2eaa4dc4b7db94bed7f09b782a7554d
MuseGlobal <aurelian.popescu@museglobal.ro>
2023-01-10 18:12:46 +0200

    code

---
+++ src/main/java/com/spring/pipelines/SpringBootForPipelinesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.pipelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForPipelinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootForPipelinesApplication.class, args);
+	}
+
+}
