c9e5b6ece125ff9bf6bff0f75224e0c957aff772
MuseGlobal <aurelian.popescu@museglobal.ro>
2023-01-11 09:42:18 +0200

    second commit

--- src/main/java/com/spring/pipelines/SpringBootForPipelinesApplication.java
+++ src/main/java/com/spring/pipelines/SpringBootForPipelinesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootForPipelinesApplication {
 
 	public static void main(String[] args) {
+	  System.out.println("v2");
 		SpringApplication.run(SpringBootForPipelinesApplication.class, args);
 	}
 
